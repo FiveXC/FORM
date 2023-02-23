d36b8f13c27d75670ec6864962cd55c72791f759
FiveXC <120128140+FiveXC@users.noreply.github.com>
2023-02-23 10:43:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,149 +1,92 @@
-let checkboxcor= document.querySelector(".checkboxcor")
-let bolinha = document.querySelector(".bolinha")
+/*||*/
+let inputs = document.querySelectorAll(".input")
 let span = document.querySelectorAll(".span")
-let required = document.querySelectorAll(".required")
-let btnEnviar = document.querySelector(".btnEnviar")
-let fieldeset = document.querySelectorAll(".fieldset")
+let areaInput = document.querySelectorAll(".areaInputs")
+let btn = document.querySelector(".btn")
+let preenchaSpan = document.querySelector(".preenchaSpan")
 let emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-let olhoaberto1 = document.querySelector(".olhoaberto1")
-let olhoaberto2 = document.querySelector(".olhoaberto2")
 
+btn.addEventListener("click", (event)=>{
+ event.preventDefault();
 
-let corSite = localStorage.getItem("ChaveCor")
-
-checkboxcor.addEventListener("click", ()=>{
-
-if(checkboxcor.checked){
- localStorage.setItem("ChaveCor", JSON.stringify(checkboxcor.checked))
- document.body.classList.add("dark")
- bolinha.classList.add("mover")
+if(!inputs[0].value || !inputs[1].value || !inputs[2].value || !inputs[3].value){
+    valiUser()
+    valiEmail()
+    valiSenha()
+    areaInput[3].style.border = "1px solid  rgb(164, 23, 177)"
+}
+ else if(!inputs[0].value || inputs[0].value.length < 4 ){
+    erro(0)
+}
+else if(!inputs[1].value || !emailRegex.test(inputs[1].value)){
+    erro(1)
+}
+else if(!inputs[2].value || inputs[2].value.length < 8){
+    erro(2)
+}
+else if(!inputs[3].value || inputs[3].value !== inputs[2].value){
+    erro(3)
 }
 else{
- localStorage.setItem("ChaveCor", JSON.stringify(checkboxcor.checked))
- document.body.classList.remove("dark")
- bolinha.classList.remove("mover")
+    salvando()
 }
 })
 
-window.onload = function(){
-if(corSite === "true"){
-  checkboxcor.checked = true
-  document.body.classList.add("dark")
-  bolinha.classList.add("mover")
-}
-else{
- checkboxcor.checked = false
- document.body.classList.remove("dark")
- bolinha.classList.remove("mover")
-}
-   
-}
 
-
-function valiNome(){
-    if(required[0].value.length < 4 ){
-        erro(0)
-    }else{
-        resolvidos(0)
-    }
-}
-function valiEmail(){
-    if(!emailRegex.test(required[1].value)){
-         erro(1)  
-    }else{
-         resolvidos(1)     
-    }
-}
-
-function valiSenha(){
-    if(required[2].value.length < 8){
-        erro(2) 
-    }
-    else{
-        resolvidos(2) 
-    }
-}
-function valiSenhaIndentica(){
-    if(required[3].value !== required[2].value){
-        erro(3) 
-    }
-    else{
-        resolvidos(3) 
-    }
-}
-function erro(index){
- span[index].style.display = "block"
- fieldeset[index].style.color = "red"
-}
-function resolvidos(index){
-    span[index].style.display = "none"
-    fieldeset[index].style.color = ""
-}
-
-btnEnviar.addEventListener("click", todosCamposVazios)
-
-function todosCamposVazios(event){
- event.preventDefault()
-
- if(!required[0].value && !required[1].value && !required[2].value && !required[3].value ){
-    valiNome()
-    valiEmail()
-    valiSenha()
-}
-else if(!required[0].value || required[0].value.length < 4){
-    valiNome()
-}
-else if(!required[1].value || !emailRegex.test(required[1].value)){
-    valiEmail()
-}
-else if(!required[2].value || required[2].value.length <8){
-    valiSenha()
-}
-else if(required[3].value !== required[2].value){
-    valiSenhaIndentica()
-}
-else{
-    salvar()
-}
-}
-
-let pegandoChave = JSON.parse(localStorage.getItem("Chave")) || []
-
-function salvar(){
+let pegandoChave = JSON.parse(localStorage.getItem("chave")) || []
+function salvando(){
 
 let dados = {
-    User: required[0].value,
-    Email: required[1].value,
-    Senha: required[2].value
+nmrInd: pegandoChave.length +1,
+Username: inputs[0].value,
+Email: inputs[1].value,
+Senha: inputs[2].value,
 }
 
 pegandoChave.push(dados)
-localStorage.setItem("Chave", JSON.stringify(pegandoChave))
+localStorage.setItem("chave", JSON.stringify(pegandoChave))
 location.href = "login/index2.html"
 }
 
-
-
-
-olhoaberto1.addEventListener("click", ()=>{
-if(required[2].type == "password"){
-required[2].type = "text"
-olhoaberto1.style.color = "#634591"
+function valiUser(){
+if(inputs[0].value.length < 4){
+    erro(0)
+}
+  else{
+    resolvido(0)
+  }
+}
+function valiEmail(){
+if(!emailRegex.test(inputs[1].value)){
+    erro(1)  
+}else{
+    resolvido(1)     
+}
+}
+function valiSenha(){
+if(inputs[2].value.length < 8){
+    erro(2) 
 }
 else{
-required[2].type = "password"
-olhoaberto1.style.color = "#A970FF"
+    resolvido(2) 
 }
-})
-    
-olhoaberto2.addEventListener("click", ()=>{
-if(required[3].type == "password"){
-required[3].type = "text"
-olhoaberto2.style.color = "#634591"
+}
+function valiSenhaIndentica(){
+if(inputs[3].value !== inputs[2].value){
+ erro(3) 
 }
 else{
-required[3].type = "password"
-olhoaberto2.style.color = "#A970FF"
+ resolvido(3) 
 }
-})
+}
+
+function erro(index){
+span[index].style.display = "block"
+areaInput[index].style.border = "1px solid  rgb(164, 23, 177)"
+}
+
+function resolvido(index){
+span[index].style.display = ""
+areaInput[index].style.border = ""
+}
     
